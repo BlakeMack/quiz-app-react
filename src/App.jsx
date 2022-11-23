49142d408df5d45ae44f263133b859b3945e4b3d
Blake McKenzie <blakemckenzie365@gmail.com>
2022-11-23 13:00:16 +0000

    quiz api data displayed on to page through quiz components

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,38 @@ function App() {
   const [start, setStart] = useState(false)
   const [quizData, setQuizData] = useState({})
   const [questions, setQuestions] = useState([1, 2, 3, 4, 5])
+  const [selected, setSelected] = useState([])
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
     .then(res => res.json())
-    .then(data => setQuizData(data))
-    console.log(quizData.results)
+    .then(data => setQuizData(data.results))
+    console.log(quizData)
   }, [start])
 
   function startQuiz () {
     setStart(true)
   }
-  const quizElements = questions.map((question, index) =>
-    <Quiz key={index} start={start}/>
-  );
+
+  function selectAnswer () {
+
+  }
+
+  function chooseAnswer() {
+    setSelected(prev => prev.push())
+  }
+
+
+  const quizElements = start && quizData.map((result, index) => {
+    result.incorrect_answers.push(result.correct_answer);
+    const answers = result.incorrect_answers;
+    console.log(answers);
+    let shuffledAnswers = answers.sort(() => Math.random() - 0.5);
+    return (
+      <Quiz key={index} start={start} question={result.question} answers={shuffledAnswers} isSelected={false}/>
+    )
+  });
+
 
   const styles = {
     height: start ? "" : "800px"
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,13 +1,17 @@
-export default function Quiz () {
+export default function Quiz (props) {
+
+  const styles = {
+    backgroundColor: props.isSelected ? "#D6DBF5" : "#F5F7FB"
+}
 
   return (
     <div className="quiz">
-      <h3 className="quiz-question">How would one say goodbye in Spanish?</h3>
+      <h3 className="quiz-question">{props.question}</h3>
       <div className="answers">
-        <button className="btn-answer">Hola</button>
-        <button className="btn-answer">Hola</button>
-        <button className="btn-answer">Hola</button>
-        <button className="btn-answer">Hola</button>
+        <button className="btn-answer" style={styles}>{props.answers[0]}</button>
+        <button className="btn-answer" style={styles}>{props.answers[1]}</button>
+        <button className="btn-answer" style={styles}>{props.answers[2]}</button>
+        <button className="btn-answer" style={styles}>{props.answers[3]}</button>
       </div>
       <hr className="break-question"/>
     </div>

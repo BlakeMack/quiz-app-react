7d3ae9cb66c13cec78facc5b413703d647e5c713
Blake McKenzie <blakemckenzie365@gmail.com>
2022-11-29 11:35:00 +0000

    api called onclick, api data displayed on page

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,30 @@ import Quiz from './components/Quiz'
 
 function App() {
   const [start, setStart] = useState(false)
-  const [quizData, setQuizData] = useState({})
-  const [questions, setQuestions] = useState([1, 2, 3, 4, 5])
+  const [quizData, setQuizData] = useState(false)
+  const [results, setResults] = useState([])
   const [selected, setSelected] = useState([])
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
     .then(res => res.json())
-    .then(data => setQuizData(data.results))
-    console.log(quizData)
+    .then(data => setQuizData(data.results));
   }, [start])
+
+
+  // Goal: implement the isselcted functionality to the quiz component
+  // method: add an iselected and iscorrect to each objects question
+  // consider changing this second use effect or removing it
+  // look into .map changing state unintentionally
+  // try to implement the isSelected feature
+  // consider the dice tenzies, for every die on page, there was a dice object, in this case there are multiple quiz objects containing questions
+  // consider adding an isselected to the quiz data state directly, if not in a seperate quiz question state
+  useEffect(() => {
+    quizData &&
+    quizData.map((quiz) => console.log(quiz))
+  }, [quizData])
+
+
 
   function startQuiz () {
     setStart(true)
@@ -25,17 +39,15 @@ function App() {
   }
 
   function chooseAnswer() {
-    setSelected(prev => prev.push())
+
   }
 
 
-  const quizElements = start && quizData.map((result, index) => {
-    result.incorrect_answers.push(result.correct_answer);
-    const answers = result.incorrect_answers;
-    console.log(answers);
+  const quizElements = start && quizData && quizData.map((quiz, index) => {
+    const answers = [...quiz.incorrect_answers, quiz.correct_answer]
     let shuffledAnswers = answers.sort(() => Math.random() - 0.5);
     return (
-      <Quiz key={index} start={start} question={result.question} answers={shuffledAnswers} isSelected={false}/>
+      <Quiz key={index} start={start} question={quiz.question} answers={shuffledAnswers} isSelected={false}/>
     )
   });
 
